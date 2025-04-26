e8093e2e3bde56e5de0b73426c8e9712e90c0ba7
tabae <tabae@users.noreply.github.com>
2025-04-26 18:48:20 +0900

    1428.7800: impl bfs

--- run_seed_zero.sh
+++ run_seed_zero.sh
@@ -1,2 +1,2 @@
-./a.out < tools/in/0000.txt > tools/out/0000.txt
-tools/target/release/vis tools/in/0000.txt tools/out/0000.txt
+./a.out < tools/in/0001.txt > tools/out/0001.txt
+tools/target/release/vis tools/in/0001.txt tools/out/0001.txt
--- src/state.hpp
+++ src/state.hpp
@@ -6,6 +6,7 @@
 #include <numeric>
 #include <algorithm>
 #include <cassert>
+#include <queue>
 
 using namespace std;
 using namespace common;
@@ -133,147 +134,91 @@ State State::generateState(const State& input_state) {
     vector is_block(n, vector<bool>(n, false));
     for(int goal = 1; goal < m; ++goal) {        
         auto [gi, gj] = goals[goal];
-        int min_ops = 1<<30;
-        vector<pair<char,char>> best_ops;
-        string dirs_i = "UDX";
-        string dirs_j = "LRX";
-        for(char dir_i: dirs_i) {
-            for(char dir_j: dirs_j) {
-                for(int delta_i = -2; delta_i <= 2; ++delta_i) {
-                    for(int delta_j = -2; delta_j <= 2; ++delta_j) {
-
-                        int org_i = i;
-                        int org_j = j;
-
-                        i = i + delta_i;
-                        j = j + delta_j;
-                        if(i < 0 || i >= n || j < 0 || j >= n) {
-                            i = org_i;
-                            j = org_j;
-                            continue;
-                        }
-
-                        vector<pair<char, char>> ops;
-                        
-                        // move to i, j
-                        {
-                            int ti = org_i, tj = org_j;
-                            while(ti != i) {
-                                if(ti < i) {
-                                    if(is_block[ti+1][tj]) {
-                                        ops.push_back({'A', 'D'});
-                                    }
-                                    ops.push_back({'M', 'D'});
-                                    ++ti;
-                                } else if(ti > i) {
-                                    if(is_block[ti-1][tj]) {
-                                        ops.push_back({'A', 'U'});
-                                    }
-                                    ops.push_back({'M', 'U'});
-                                    --ti;
-                                }
-                            }
-                            while(tj != j) {
-                                if(tj < j) {
-                                    if(is_block[ti][tj+1]) {
-                                        ops.push_back({'A', 'R'});
-                                    }
-                                    ops.push_back({'M', 'R'});
-                                    ++tj;
-                                } else if(tj > j) {
-                                    if(is_block[ti][tj-1]) {
-                                        ops.push_back({'A', 'L'});
-                                    }
-                                    ops.push_back({'M', 'L'});
-                                    --tj;
-                                }
-                            }     
-                        }
-                        
-                        if(dir_i != 'X') {
-                            int ni = i + common::dij(dir_i).first;
-                            if(ni >= 0 && ni < n) {
-                                ops.push_back({'S', dir_i});
-                            }
-                        }
-                        if(dir_j != 'X') {
-                            int nj = j + common::dij(dir_j).second;
-                            if(nj >= 0 && nj < n) {
-                                ops.push_back({'S', dir_j});
-                            }
-                        }
-                        vector<pair<int,int>> _used_block;
-                        auto [ti, tj] = common::exec_operations(org_i, org_j, ops, is_block, false, _used_block);
-
-                        while(ti != gi) {
-                            if(ti < gi) {
-                                if(is_block[ti+1][tj]) {
-                                    ops.push_back({'A', 'D'});
-                                }
-                                ops.push_back({'M', 'D'});
-                                ++ti;
-                            } else if(ti > gi) {
-                                if(is_block[ti-1][tj]) {
-                                    ops.push_back({'A', 'U'});
-                                }
-                                ops.push_back({'M', 'U'});
-                                --ti;
-                            }
-                        }
-                        while(tj != gj) {
-                            if(tj < gj) {
-                                if(is_block[ti][tj+1]) {
-                                    ops.push_back({'A', 'R'});
-                                }
-                                ops.push_back({'M', 'R'});
-                                ++tj;
-                            } else if(tj > gj) {
-                                if(is_block[ti][tj-1]) {
-                                    ops.push_back({'A', 'L'});
-                                }
-                                ops.push_back({'M', 'L'});
-                                --tj;
-                            }
-                        }     
-                        if(ops.size() < min_ops) {
-                            min_ops = ops.size();
-                            best_ops = ops;
-                        }
-
-                        i = org_i;
-                        j = org_j;
+        vector<vector<bool>> seen(n, vector<bool>(n, false));
+        vector<vector<tuple<int,int,char,char>>> from(n, vector<tuple<int,int,char,char>>(n));
+        queue<pair<int,int>> que;
+        seen[i][j] = true;
+        que.push({i, j});
+        string commands = "MS";
+        string dirs = "UDLR";
+        while(!que.empty()) {
+            auto [ci, cj] = que.front();
+            que.pop();
+            for(char com: commands) {
+                for(char dir: dirs) {
+                    auto [di, dj] = common::dij(dir);
+                    int ni = ci, nj = cj;
+                    if(com == 'M') {
+                        ni = ci + di;
+                        nj = cj + dj;
                     }
+                    if(com == 'S') {
+                        int nni = ci;
+                        int nnj = cj;
+                        while(true) {
+                            nni = nni + di;
+                            nnj = nnj + dj;
+                            if(nni < 0 || nni >= n || nnj < 0 || nnj >= n) {
+                                break;
+                            }
+                            if(is_block[nni][nnj]) {
+                                break;
+                            }
+                            ni = nni;
+                            nj = nnj;
+                        }
+                    }
+                    if(ni < 0 || ni >= n || nj < 0 || nj >= n) continue;
+                    if(seen[ni][nj]) continue;
+                    if(is_block[ni][nj]) continue;
+                    seen[ni][nj] = true;
+                    que.push({ni, nj});
+                    from[ni][nj] = {ci, cj, com, dir};
                 }
-
             }
         }
+        if (!seen[gi][gj]) {
+            res.score = -1;
+            return res;
+        }
+
+        // restore operations
+        vector<pair<char,char>> best_ops;
+        int ci = gi, cj = gj;
+        while(ci != i || cj != j) {
+            auto [ni, nj, com, dir] = from[ci][cj];
+            best_ops.push_back({com, dir});
+            ci = ni;
+            cj = nj;
+        }
+        reverse(best_ops.begin(), best_ops.end());
 
         i = gi;
         j = gj;
 
         if(goal <= num_blocks) {
-            int d = ryuka.rand(4);
             string dirs = "LRUD";
-            char dir = dirs[d];
-            int ni = i + common::dij(dir).first;
-            int nj = j + common::dij(dir).second;
-            if(ni >= 0 && ni < n && nj >= 0 && nj < n) {
-                best_ops.push_back({'A', dir});
+            shuffle(dirs.begin(), dirs.end(), ryuka.engine);
+            for(char dir: dirs) {
+                int ni = i + common::dij(dir).first;
+                int nj = j + common::dij(dir).second;
+                if(ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                    bool ok = true;
+                    for(int k = goal+1; k < m; ++k) {
+                        if(ni == goals[k].first && nj == goals[k].second) {
+                            ok = false;
+                            break;
+                        }
+                    }
+                    if(!ok) continue;
+                    best_ops.push_back({'A', dir});
+                    break;
+                }  
             }
         }
         for(auto op: best_ops) {
             res.operations.push_back(op);
         }
-
-        /*
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
-                cerr << is_block[i][j] << " ";
-            }
-            cerr << endl;
-        }
-        cerr << "aaa" << endl;
-        */
         vector<pair<int,int>> _used_block;
         auto [si, sj] = goals[goal-1];
         tie(i, j) = common::exec_operations(si, sj, best_ops, is_block, true, _used_block);
